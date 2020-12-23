d9c06ce4600e59e2fe4f46da83927241bd607604
Minh Vu <minhd_vu@yahoo.com>
2020-12-23 09:57:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# pretty-pandoc
+
+Generate beautiful pdfs from github markdown files using pandoc.
+
+Source: (https://learnbyexample.github.io/customizing-pandoc/)[https://learnbyexample.github.io/customizing-pandoc/]
---
+++ pandoc-script.sh
@@ -0,0 +1,15 @@
+pandoc "$1" \
+    -f gfm \
+    --include-in-header chapter_break.tex \
+    -V linkcolor:blue \
+    -V geometry:a4paper \
+    -V geometry:margin=2cm \
+    -V mainfont="DejaVu Serif" \
+    -V monofont="DejaVu Sans Mono" \
+    --pdf-engine=xelatex \
+    --highlight-style pygments.theme \
+    --include-in-header inline_code.tex \
+    --include-in-header bullet_style.tex \
+    --include-in-header quote.tex \
+    --toc \
+    -o "$2"
